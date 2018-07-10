a701444d5b2c0653e3c3a535fc949de40c1c8a13
Ali Parlakci <parlakciali@gmail.com>
2018-07-10 03:14:43 +0300

    Updated the version info

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ from src.errors import *
 
 __author__ = "Ali Parlakci"
 __license__ = "GPL"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __maintainer__ = "Ali Parlakci"
 __email__ = "parlakciali@gmail.com"
 
