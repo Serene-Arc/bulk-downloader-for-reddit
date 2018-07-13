9f831e1b784a770c82252e909462871401a05c11
Ali Parlakci <parlakciali@gmail.com>
2018-07-13 14:12:17 +0300

    Added .exe to executable's extension

--- setup.py
+++ setup.py
@@ -3,13 +3,48 @@
 ## python setup.py build
 import sys
 from cx_Freeze import setup, Executable
+from script import __version__
+
+options = {
+    "build_exe": {
+        "packages":[
+            "idna","imgurpython", "praw", "requests"
+        ]
+    }
+}
+
+if sys.platform == "win32":
+    executables = [Executable(
+        "script.py", 
+        targetName="bulk-downloader-for-reddit.exe",
+        shortcutName="Bulk Downloader for Reddit",
+        shortcutDir="DesktopFolder"
+    )]
+
+elif sys.platform == "linux":
+    executables = [Executable(
+        "script.py", 
+        targetName="bulk-downloader-for-reddit",
+        shortcutName="Bulk Downloader for Reddit",
+        shortcutDir="DesktopFolder"
+    )]
 
 setup(
-        name = "Bulk Downloader for Reddit",
-        version = "1.1.0",
-        description = "Bulk Downloader for Reddit",
-        author = "Ali Parlakci",
-        executables = [Executable("script.py")],
-        options = {"build_exe": {"packages":["idna","imgurpython", "praw", "requests"]}})
+    name = "Bulk Downloader for Reddit",
+    version = __version__,
+    description = "Bulk Downloader for Reddit",
+    author = "Ali Parlakci",
+    author_email="parlakciali@gmail.com",
+    url="https://github.com/aliparlakci/bulk-downloader-for-reddit",
+    classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+            "Natural Language :: English",
+            "Environment :: Console",
+            "Operating System :: OS Independent",
+    ),
+    executables = executables,
+    options = options
+)
 
 
