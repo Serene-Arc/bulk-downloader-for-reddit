8ac02e7affa15c47f29cc9a05319582b6da7f864
Ali Parlakci <parlakciali@gmail.com>
2018-07-24 19:42:38 +0300

    Update version

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ from src.tools import (GLOBAL, createLogFile, jsonFile, nameCorrector,
 
 __author__ = "Ali Parlakci"
 __license__ = "GPL"
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 __maintainer__ = "Ali Parlakci"
 __email__ = "parlakciali@gmail.com"
 
