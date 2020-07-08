9d2b92f37014adad81e2f6fdb64bd16926941244
Ali <parlakciali@gmail.com>
2020-07-08 22:24:25 +0300

    Version iteration

--- script.py
+++ script.py
@@ -38,7 +38,7 @@ from src.store import Store
 
 __author__ = "Ali Parlakci"
 __license__ = "GPL"
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 __maintainer__ = "Ali Parlakci"
 __email__ = "parlakciali@gmail.com"
 
