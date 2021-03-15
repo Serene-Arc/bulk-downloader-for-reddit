4cd25bcad9c8941e2423a49722cad5fa3c842b3f
Serene-Arc <serenical@gmail.com>
2021-03-15 10:44:29 +1000

    Add basic setup files

--- setup.py
+++ setup.py
@@ -1,50 +1,6 @@
-#!C:\Users\Ali\AppData\Local\Programs\Python\Python36\python.exe
+#!/usr/bin/env python3
+# encoding=utf-8
 
-# python setup.py build
-import sys
+from setuptools import setup
 
-from cx_Freeze import Executable, setup
-
-from bulkredditdownloader.__main__ import __version__
-
-options = {
-    "build_exe": {
-        "packages": [
-            "idna", "praw", "requests", "multiprocessing"
-        ]
-    }
-}
-
-if sys.platform == "win32":
-    executables = [Executable(
-        "script.py",
-        targetName="bulk-downloader-for-reddit.exe",
-        shortcutName="Bulk Downloader for Reddit",
-        shortcutDir="DesktopFolder"
-    )]
-
-elif sys.platform == "linux":
-    executables = [Executable(
-        "script.py",
-        targetName="bulk-downloader-for-reddit",
-        shortcutName="Bulk Downloader for Reddit",
-        shortcutDir="DesktopFolder"
-    )]
-
-setup(
-    name="Bulk Downloader for Reddit",
-    version=__version__,
-    description="Bulk Downloader for Reddit",
-    author="Ali Parlakci",
-    author_email="parlakciali@gmail.com",
-    url="https://github.com/aliparlakci/bulk-downloader-for-reddit",
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-        "Natural Language :: English",
-        "Environment :: Console",
-        "Operating System :: OS Independent",
-    ),
-    executables=executables,
-    options=options
-)
+setup(setup_requires=['pbr', 'appdirs'], pbr=True, data_files=[('config', ['bulkredditdownloader/default_config.cfg'])])
