77bdbbac634cb6126de9f4bac839f9457aa7d81d
Serene-Arc <serenical@gmail.com>
2021-04-12 19:00:12 +1000

    Update test hash

--- bdfr/tests/site_downloaders/test_youtube.py
+++ bdfr/tests/site_downloaders/test_youtube.py
@@ -12,7 +12,7 @@ from bdfr.site_downloaders.youtube import Youtube
 @pytest.mark.online
 @pytest.mark.slow
 @pytest.mark.parametrize(('test_url', 'expected_hash'), (
-    ('https://www.youtube.com/watch?v=uSm2VDgRIUs', '3c79a62898028987f94161e0abccbddf'),
+    ('https://www.youtube.com/watch?v=uSm2VDgRIUs', 'f70b704b4b78b9bb5cd032bfc26e4971'),
     ('https://www.youtube.com/watch?v=m-tKnjFwleU', '30314930d853afff8ebc7d8c36a5b833'),
 ))
 def test_find_resources(test_url: str, expected_hash: str):
