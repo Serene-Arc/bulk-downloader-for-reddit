aea30d2b44f6099645a70341ba508f853c167e23
Ali Parlakçı <parlakciali@gmail.com>
2021-05-15 13:11:01 +0300

    Check if REDDIT_TOKEN is set (#376)

--- devscripts/configure.sh
+++ devscripts/configure.sh
@@ -1,2 +1,5 @@
-cp ./bdfr/default_config.cfg ./test_config.cfg
-echo -e "\nuser_token = $REDDIT_TOKEN" >> ./test_config.cfg
+if [ ! -z "$REDDIT_TOKEN" ]
+then
+    cp ./bdfr/default_config.cfg ./test_config.cfg
+    echo -e "\nuser_token = $REDDIT_TOKEN" >> ./test_config.cfg
+fi
