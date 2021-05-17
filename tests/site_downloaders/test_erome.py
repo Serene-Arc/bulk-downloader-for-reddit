71da1556e535a46a33f9ade5fb90119c57a19ce5
Serene-Arc <serenical@gmail.com>
2021-05-17 11:08:16 +1000

    Change out of date test case

--- tests/site_downloaders/test_erome.py
+++ tests/site_downloaders/test_erome.py
@@ -34,9 +34,6 @@ def test_get_link(test_url: str, expected_urls: tuple[str]):
     ('https://www.erome.com/a/vqtPuLXh', {
         '5da2a8d60d87bed279431fdec8e7d72f'
     }),
-    ('https://www.erome.com/i/ItASD33e', {
-        'b0d73fedc9ce6995c2f2c4fdb6f11eff'
-    }),
     ('https://www.erome.com/a/lGrcFxmb', {
         '0e98f9f527a911dcedde4f846bb5b69f',
         '25696ae364750a5303fc7d7dc78b35c1',
@@ -44,6 +41,12 @@ def test_get_link(test_url: str, expected_urls: tuple[str]):
         'a1abf398cfd4ef9cfaf093ceb10c746a',
         'bd9e1a4ea5ef0d6ba47fb90e337c2d14'
     }),
+    ('https://www.erome.com/a/IK5HADyi', {
+        '3b2a441ff821c09d9b629271a8b0f19f',
+        '470343fa67fd2ef9687c4223d278f761',
+        '7fbbc092939919aa74a710ddd26adc02',
+        'c7299a73e019ab635b47c863fe3cd473',
+    })
 ))
 def test_download_resource(test_url: str, expected_hashes: tuple[str]):
     # Can't compare hashes for this test, Erome doesn't return the exact same file from request to request so the hash
