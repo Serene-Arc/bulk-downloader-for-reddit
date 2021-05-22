5aae6b3df88b21216fc1a756b98f5a21fbe9607a
Serene-Arc <serenical@gmail.com>
2021-05-22 11:53:12 +1000

    Add another message type

--- scripts/extract_successful_ids.sh
+++ scripts/extract_successful_ids.sh
@@ -19,4 +19,5 @@ fi
     grep 'Resource hash' "$file" | awk '{ print $(NF-2) }' ;
     grep 'Download filter' "$file" | awk '{ print $(NF-3) }' ;
     grep 'already exists, continuing' "$file" | awk '{ print $(NF-3) }' ;
+    grep 'Hard link made' "$file" | awk '{ print $(NF) }' ;
 } >> "$output"
