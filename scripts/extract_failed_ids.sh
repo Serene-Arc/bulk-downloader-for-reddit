9fd8b29833fa5f87ebdb6c4786f6489e79f1c297
Serene-Arc <serenical@gmail.com>
2021-06-11 18:36:40 +1000

    Add another logging message to script

--- scripts/extract_failed_ids.sh
+++ scripts/extract_failed_ids.sh
@@ -19,4 +19,5 @@ fi
     grep 'Failed to download resource' "$file" | awk '{ print $15 }' ;
     grep 'failed to download submission' "$file" | awk '{ print $14 }' | rev | cut -c 2- | rev ;
     grep 'Failed to write file' "$file" | awk '{ print $16 }' | rev | cut -c 2- | rev ;
+    grep 'skipped due to disabled module' "$file" | awk '{ print $9 }' ;
 } >>"$output"
