91fa9919e086c600f957c03f9c76a5428c14daca
Erik Johnson <source@ekriirke.net>
2024-11-24 14:22:03 +0100

    Retry the current action 5x every 60s pause instead of skipping to the next

--- bdfr/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-__version__ = "2.7.0"
--- devscripts/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n "$REDDIT_TOKEN" ]
-then
-    cp ./bdfr/default_config.cfg ./tests/test_config.cfg
-    echo -e "\nuser_token = $REDDIT_TOKEN" >> ./tests/test_config.cfg
-fi
