00ea4fd391591109f5699e219c848e4c075f6f9e
Erik Johnson <source@ekriirke.net>
2024-11-24 14:22:03 +0100

    Retry the current action 5x every 60s pause instead of skipping to the next

--- bdfr/site_authenticator.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import configparser
-
-
-class SiteAuthenticator:
-    def __init__(self, cfg: configparser.ConfigParser) -> None:
-        self.imgur_authentication = None
--- devscripts/configure.ps1
+++
@@ -1,5 +0,0 @@
-if (-not ([string]::IsNullOrEmpty($env:REDDIT_TOKEN)))
-{
-    Copy-Item .\\bdfr\\default_config.cfg .\\tests\\test_config.cfg
-    Write-Output "`nuser_token = $env:REDDIT_TOKEN" >> ./tests/test_config.cfg
-}
