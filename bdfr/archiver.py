04bd7e24fc18e3c9ace0ca55dd140ba6a52e1488
Erik Johnson <source@ekriirke.net>
2025-08-12 20:27:03 +0200

    Resync

--- bdfr/archiver.py
+++ bdfr/archiver.py
@@ -31,30 +31,33 @@ class Archiver(RedditConnector):
 
     def download(self):
         for generator in self.reddit_lists:
-            for retry in range(5):
-                try:
-                    for submission in generator:
-                        try:
-                            if (submission.author and submission.author.name in self.args.ignore_user) or (
-                                submission.author is None and "DELETED" in self.args.ignore_user
-                            ):
-                                logger.debug(
-                                    f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
-                                    f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
-                                )
-                                continue
-                            if submission.id in self.excluded_submission_ids:
-                                logger.debug(f"Object {submission.id} in exclusion list, skipping")
-                                continue
-                            logger.debug(f"Attempting to archive submission {submission.id}")
-                            self.write_entry(submission)
-                        except prawcore.PrawcoreException as e:
-                            logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
-                    break
-                except prawcore.PrawcoreException as e:
-                    logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
-                    logger.debug("Waiting 60 seconds to continue")
-                    sleep(60)
+            for submission in generator:
+                current_wait_time = 0
+                while True:
+                    try:
+                        if (submission.author and submission.author.name in self.args.ignore_user) or (
+                            submission.author is None and "DELETED" in self.args.ignore_user
+                        ):
+                            logger.debug(
+                                f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
+                                f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
+                            )
+                            break
+                        if submission.id in self.excluded_submission_ids:
+                            logger.debug(f"Object {submission.id} in exclusion list, skipping")
+                            break
+                        logger.debug(f"Attempting to archive submission {submission.id}")
+                        self.write_entry(submission)
+                        break
+                    except prawcore.PrawcoreException as e:
+                        logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
+                        if current_wait_time < self.args.max_wait_time:
+                            current_wait_time += self.args.wait_time_interval
+                            logger.debug(f"Waiting {current_wait_time} seconds to continue")
+                            sleep(current_wait_time)
+                        else:
+                            logger.error(f"Max wait time exceeded for submission {submission.id}")
+                            raise
 
     def get_submissions_from_link(self) -> list[list[praw.models.Submission]]:
         supplied_submissions = []
---
+++ bdfr/configuration.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+from argparse import Namespace
+from pathlib import Path
+from typing import Optional
+
+import click
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+class Configuration(Namespace):
+    def __init__(self):
+        super(Configuration, self).__init__()
+        self.authenticate = False
+        self.config = None
+        self.opts: Optional[str] = None
+        self.directory: str = "."
+        self.disable_module: list[str] = []
+        self.exclude_id = []
+        self.exclude_id_file = []
+        self.file_scheme: str = "{REDDITOR}_{TITLE}_{POSTID}"
+        self.filename_restriction_scheme = None
+        self.folder_scheme: str = "{SUBREDDIT}"
+        self.ignore_user = []
+        self.include_id_file = []
+        self.limit: Optional[int] = None
+        self.link: list[str] = []
+        self.log: Optional[str] = None
+        self.make_hard_links = False
+        self.max_wait_time = None
+        self.wait_time_interval = 60
+        self.multireddit: list[str] = []
+        self.no_dupes: bool = False
+        self.saved: bool = False
+        self.search: Optional[str] = None
+        self.search_existing: bool = False
+        self.skip: list[str] = []
+        self.skip_domain: list[str] = []
+        self.skip_subreddit: list[str] = []
+        self.min_score = None
+        self.max_score = None
+        self.min_score_ratio = None
+        self.max_score_ratio = None
+        self.sort: str = "hot"
+        self.submitted: bool = False
+        self.subscribed: bool = False
+        self.subreddit: list[str] = []
+        self.time: str = "all"
+        self.time_format = None
+        self.upvoted: bool = False
+        self.user: list[str] = []
+        self.verbose: int = 0
+
+        # Archiver-specific options
+        self.all_comments = False
+        self.format = "json"
+        self.comment_context: bool = False
+
+    def process_click_arguments(self, context: click.Context):
+        if context.params.get("opts") is not None:
+            self.parse_yaml_options(context.params["opts"])
+        for arg_key in context.params.keys():
+            if not hasattr(self, arg_key):
+                logger.warning(f"Ignoring an unknown CLI argument: {arg_key}")
+                continue
+            val = context.params[arg_key]
+            if val is None or val == ():
+                # don't overwrite with an empty value
+                continue
+            setattr(self, arg_key, val)
+
+    def parse_yaml_options(self, file_path: str):
+        yaml_file_loc = Path(file_path)
+        if not yaml_file_loc.exists():
+            logger.error(f"No YAML file found at {yaml_file_loc}")
+            return
+        with yaml_file_loc.open() as file:
+            try:
+                opts = yaml.safe_load(file)
+            except yaml.YAMLError as e:
+                logger.error(f"Could not parse YAML options file: {e}")
+                return
+        for arg_key, val in opts.items():
+            if not hasattr(self, arg_key):
+                logger.warning(f"Ignoring an unknown YAML argument: {arg_key}")
+                continue
+            setattr(self, arg_key, val)
