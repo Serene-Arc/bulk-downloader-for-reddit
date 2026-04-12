1236cf2fd23a834230ea71a16e2e1ff274f4d0b3
Erik Johnson <source@ekriirke.net>
2026-04-12 17:53:37 +0200

    OAuth2 revival

--- bdfr/connector.py
+++ bdfr/connector.py
@@ -163,6 +163,7 @@ class RedditConnector(metaclass=ABCMeta):
                 client_id=client_id,
                 client_secret=client_secret,
                 user_agent=self.user_agent,
+                refresh_token=self.cfg_parser.get("DEFAULT", "user_token"),
                 ratelimit_seconds=120,
             )
         else:
--- bdfr/oauth2.py
+++ bdfr/oauth2.py
@@ -51,7 +51,7 @@ class OAuth2Authenticator:
             client_secret=self.client_secret,
         )
         state = str(random.randint(0, 65000))  # noqa: S311
-        url = reddit.auth.url(self.scopes, state, "permanent")
+        url = reddit.auth.url(scopes=self.scopes, state=state, duration="permanent")
         logger.warning("Authentication action required before the program can proceed")
         logger.warning(f"Authenticate at {url}")
 
